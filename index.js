9402d477a0bf30ea9acc69e5ed84a54e634cea45
yoitsro <yoitsro@hotmail.com>
2014-03-26 21:25:38 +0000

    Fixed index.js to work nicely locally

--- index.js
+++ index.js
@@ -1,44 +1,37 @@
 var Hapi     = require('hapi');
-var mongoose = require('mongoose');
-var Hawk     = require('hawk');
+var Mongoose = require('mongoose');
 var Auth     = require('./lib/auth')();
-
-var Fs       = require('fs');
-
-require('./models');
-var mongoose = require('mongoose');
-
-
-process.env.PAGINATE_LIMIT_MAX = 500;
-process.env.PAGINATE_LIMIT     = 100;
-process.env.SELECT_FIELDS_USER = "_id name registered image";
-
 
 var config = {
     hostname: 'localhost',
-    port: +process.env.npm_package_config_PORT || 9001
+    port: +process.env.PORT || 9001
 };
 
 // Create a server with a host and port
 var server = Hapi.createServer(config.hostname, config.port, {cors: true});
 
 server.pack.require(['hapi-auth-hawk'], function (err) {
-    server.auth.strategy('hawk', 'hawk', { getCredentialsFunc: Auth.getCredentials, hawk:{port: 443}});
+    server.auth.strategy('hawk', 'hawk', { getCredentialsFunc: Auth.getCredentials, hawk: {port: process.env.NODE_ENV === 'production' ? 443 : config.port}});
 });
 
 server.pack.require(['./routes/users', './routes/auth', './routes/teams', './routes/teams/members', './routes/teams/rounds', './routes/teams/rounds/orders'], function (err) {
 
 });
 
-mongoose.connect(process.env.npm_package_config_MONGO_URL, {db: { native_parser: true }}, function(err) {
+Mongoose.connect(process.env.MONGO_URL, {db: { native_parser: true }}, function(err) {
     if (err) {
         throw err;
     }
 
     console.log("Connected to MongoDB");
     server.start(function() {
+        var uri = "";
 
-        var uri = process.env.HOST ? 'https://' + process.env.HOST + ':' + process.env.PORT : 'https://' + config.hostname + ':' + config.port;
+        if(process.env.NODE_ENV === 'production') {
+            uri = 'https://' + process.env.HOSTNAME + ':' + process.env.PORT;
+        } else {
+            uri = 'http://' + config.hostname + ':' + config.port;
+        }
         console.log('Server started at ' + uri);
         
     });
